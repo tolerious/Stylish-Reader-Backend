0ffb9dc6f99be4e8824eb5d71dd9df453f59e84e
He Long <tolerious@qq.com>
2023-08-08 19:54:10 +0800

    :construction: Update code.

--- routes/smsCode.js
+++ routes/smsCode.js
@@ -1,7 +1,6 @@
 var express = require("express");
 const { generateResponse, createClient } = require("../utils/utils");
 const { smsCodeModel } = require("../schemas/smsCodeSchema");
-const { default: Client } = require("../utils/client");
 var router = express.Router();
 /* GET home page. */
 router.post("/", async function (req, res, next) {
@@ -9,7 +8,6 @@ router.post("/", async function (req, res, next) {
   let username = t.username || undefined;
   if (!username) res.json(generateResponse("", 400, "invalid username"));
   let c = await smsCodeModel.generateCode(username);
-  Client.main()
   res.json(generateResponse(c));
 });
 
