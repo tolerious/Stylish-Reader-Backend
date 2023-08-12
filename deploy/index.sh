55de103994b86a2c4ba7d799ccaed6e9883d764d
Helong <tolerious@qq.com>
2023-08-12 14:38:30 +0800

    Update index.sh

--- deploy/index.sh
+++ deploy/index.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 ls -alh
-/root/.nvm/versions/node/v16.20.0/bin/pnpm i
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias pn='pnpm'
+
+# pnpm
+export PNPM_HOME="/root/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
 
