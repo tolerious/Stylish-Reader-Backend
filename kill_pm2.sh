01692e53d149129203a7eecddc56a11644f37426
Toly <tolerious.feng@gmail.com>
2024-07-29 22:03:47 +0800

    🚧 Config pipeline.

--- kill_pm2.sh
+++ kill_pm2.sh
@@ -4,6 +4,8 @@ RUNNING_PROCESSES=$(pm2 jlist | jq '[.[] | select(.pm2_env.status == "online")] 
 
 if [ "$RUNNING_PROCESSES" -gt 0 ]; then
   pm2 stop all
+  pnpm i
+  pm2 start --env=production
   echo "Stopped all running PM2 processes."
 else
   echo "No running PM2 processes to stop."
