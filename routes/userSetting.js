2988b8c1b82b139dd338857701c9cde666642612
Toly <tolerious.feng@gmail.com>
2024-09-07 14:44:55 +0800

    🚧 Update usersetting api.

--- routes/userSetting.js
+++ routes/userSetting.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { generateResponse, grabWordFromCambridge } = require("../utils/utils");
 const { userSettingModel } = require("../schemas/userSettingsSchema");
 const router = express.Router();
-/* GET home page. */
+
+// 获取用户settings
 router.get("/", async function (req, res, next) {
   const u = req.tUser;
   let r = await userSettingModel.findOne({ userID: u._id });
   res.json(generateResponse(r));
 });
 
+// 创建或者更新用户settings
 router.post("/", async function (req, res, next) {
   let d = req.body;
   let u = req.tUser;
@@ -18,10 +20,7 @@ router.post("/", async function (req, res, next) {
     Object.assign(d, { userID: u._id });
     t = await userSettingModel.create(d);
   } else {
-    t = await userSettingModel.findOneAndUpdate(
-      { userID: u._id },
-      { defaultGroupID: d.defaultGroupID }
-    );
+    t = await userSettingModel.findOneAndUpdate({ userID: u._id }, d);
   }
   res.json(generateResponse(t));
 });
